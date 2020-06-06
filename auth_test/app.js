47bd21702e681bfa1ca48ea211edd420a3495aed
Aleksandra <al.belyaeva1@gmail.com>
2020-06-06 03:54:44 +0300

    1

--- auth_test/app.js
+++ auth_test/app.js
@@ -7,15 +7,25 @@ if(location.hash.indexOf("#access_token=")===0){
     location.hash = "";
 }
 
+    function send(method, params, callback) {
+        $.ajax({
+            url: URL(method, params),
+            method: "GET",
+            dataType: "JSONP",
+            success: callback
+        });
+    }
+
+
 if(Date.now() <= parseInt(localStorage.session)){
-    $("#btn").hide();
-    send('friends.search',{count:5, fields:'photo_200'}, function (data) {
+        $("#btn").hide();
+    send('friends.search',{count:5, fields:'photo_100'}, function (data) {
         render(data.response);
     });
     send('users.get',{}, function (data) {
         //render
         const btn = '<button type="button" onclick="localStorage.clear(); location.reload();" class="btn btn-dark">Выйти</button>';
-        $('#username').html('Привет, '+data.response[0].first_name+' '+data.response[0].last_name+'! '+ btn).show();
+        $('#username').html('+data.response[0].first_name+' + '+data.response[0].last_name+' + btn).show();
     });
 }
 
@@ -29,21 +39,12 @@ function URL  (method, params) {
     return "https://api.vk.com/method/" + method + '?' + $.param(params);
 }
 
-function send(method, params, callback) {
-    $.ajax({
-        url: URL(method, params),
-        method: "GET",
-        dataType: "JSONP",
-        success: callback
-    });
-}
-
 function render(response) {
     let html = '';
     for(let i=1; i<response.length;i++){
         let f = response[i];
         html += "<div class=\"card col-sm\">" +
-            "<img class=\"card-img-top\" src=\""+f.photo_200+"\">" +
+            "<img class=\"card-img-top\" src=\""+f.photo_100+"\">" +
             "<div class=\"card-body\">" +
             "<h5 class=\"card-text\">"+f.first_name+' '+f.last_name+"</h5>" +
             "</div>" +
