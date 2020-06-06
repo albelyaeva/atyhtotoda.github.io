934ca2e0e370a2b3d2c4722e650a8826d07e7eb8
Aleksandra <al.belyaeva1@gmail.com>
2020-06-06 04:38:56 +0300

    3

--- auth_test/app1.js
+++ auth_test/app1.js
@@ -7,24 +7,25 @@
     if(Date.now() <= parseInt(localStorage.session)) {
         $("#btn").hide();
         const token = localStorage.token;
-        var req=`https://api.vk.com/method/users.get?access_token=${token}`;
+        var req = `https://api.vk.com/method/users.get?access_token=${token}`;
         $.ajax({
-            url : req,
-            type : "GET",
-            dataType : "jsonp",
-            success : render
-        });
-    }
-        function render(response) {
-            let html = '';
-            for (let i = 1; i < response.length; i++) {
-                let f = response[i];
-                html += "<div class=\"card col-sm\">" +
-                    "<img class=\"card-img-top\" src=\"" + f.photo_200 + "\">" +
-                    "<div class=\"card-body\">" +
-                    "<h5 class=\"card-text\">" + f.first_name + ' ' + f.last_name + "</h5>" +
-                    "</div>" +
-                    "</div>";
+            url: req,
+            type: "GET",
+            dataType: "jsonp",
+            success: function(msg){
+                console.log(msg.response[0]);
             }
-        }
+    })
+    }
+        // function render(response) {
+        //     let html = '';
+        //     for(let i=1; i<response.length;i++){
+        //         let f = response[i];
+        //         html += "<div class=\"card col-sm\">" +
+        //             "<img class=\"card-img-top\" src=\""+f.photo_200+"\">" +
+        //             "<div class=\"card-body\">" +
+        //             "<h5 class=\"card-text\">"+f.first_name+' '+f.last_name+"</h5>" +
+        //             "</div>" +
+        //             "</div>";
+        //     }
 })();
